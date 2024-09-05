4a04c6665e7f0d27b47a41ea799d7e2d4df8e860
Deevanshu Kushwah <astroboiscosmos@gmail.com>
2024-09-05 20:17:08 +0530

    taskcli addTask is connected to backend

--- libs/task.go
+++ libs/task.go
@@ -1,6 +1,7 @@
 package libs
 
 import (
+	"bytes"
 	"encoding/csv"
 	"encoding/json"
 	"errors"
@@ -9,7 +10,6 @@ import (
 	"net/http"
 	"os"
 	"strconv"
-	"time"
 )
 
 type Task struct {
@@ -66,69 +66,20 @@ func AllData() ([][]string, error) {
 	return data, nil
 }
 
-func lastId() (int, error) {
-	records, err := AllData()
-	if err != nil {
-		return 0, errors.New("fail to ready db")
-	}
-	if len(records) == 0 {
-		return 0, nil
-	}
-
-	var tasks []Task
-	for _, record := range records {
-		id, _ := strconv.Atoi(record[0])
-		description := record[1]
-		var status int
-		if record[2] == Pending.String() {
-			status = int(Pending)
-		} else if record[2] == Inprogress.String() {
-			status = int(Inprogress)
-		} else {
-			status = int(Complete)
-		}
-
-		task := Task{
-			Id:          id,
-			Description: description,
-			Status:      StatusEnum(status),
-			CreatedAt:   record[3],
-		}
-		tasks = append(tasks, task)
-	}
-
-	return tasks[len(tasks)-1].Id, nil
-}
-
 func CreateTask(tasks []string, status StatusEnum) error {
-	file, err := os.OpenFile("data.csv", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	base_url := os.Getenv("BASE_URL")
+	var reqBody struct {
+		Tasks []string `json:"tasks"`
+	}
+	reqBody.Tasks = tasks
+	reqBodyBytes, err := json.Marshal(reqBody)
 	if err != nil {
-		return fmt.Errorf("error while opening the db %w", err)
+		return err
 	}
-	defer file.Close()
-
-	writer := csv.NewWriter(file)
-
-	// Data to append
-	t := time.Now()
-	id, err := lastId()
+	_, err = http.Post(base_url+"/createTasks", "application/json", bytes.NewReader(reqBodyBytes))
 	if err != nil {
-		return fmt.Errorf("error while reading the db  %w", err)
+		return err
 	}
-	newRecords := [][]string{}
-	for _, task := range tasks {
-		id = id + 1
-		newRecord := []string{fmt.Sprint(id), task, fmt.Sprint(int(status)), t.Format("02-01-2006")}
-		newRecords = append(newRecords, newRecord)
-	}
-
-	// Write the new record to the CSV file
-	err = writer.WriteAll(newRecords)
-	writer.Flush()
-	if err != nil {
-		return fmt.Errorf("failed to write record to file %w", err)
-	}
-
 	return nil
 }
 
